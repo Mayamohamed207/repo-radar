5916c73a7e6448ded9c6d92a1edb468cd5c88853
Mayamohamed207 <mayamohamed364@gmail.com>
2026-09-19 13:37:26 +0300

    wrap storybook stories in toast provider

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,7 +4,10 @@ import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import { store } from '../src/store/store'
 import { theme } from '../src/theme/theme'
+import { ToastProvider } from '../src/providers/ToastContext'
+// @ts-expect-error 
 import '../src/styles/variables.css'
+// @ts-expect-error
 import '../src/index.css'
 
 const preview: Preview = {
@@ -13,7 +16,9 @@ const preview: Preview = {
       <Provider store={store}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Story />
+          <ToastProvider>
+            <Story />
+          </ToastProvider>
         </ThemeProvider>
       </Provider>
     ),
