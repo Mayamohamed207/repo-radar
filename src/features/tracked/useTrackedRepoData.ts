51cda80fe76bc157726661d9d59099f94035d252
Mayamohamed207 <mayamohamed364@gmail.com>
2026-09-19 04:14:55 +0300

    Refactor: extract tracked repo selector into its own function

--- src/features/tracked/useTrackedRepoData.ts
+++ src/features/tracked/useTrackedRepoData.ts
@@ -1,12 +1,22 @@
-import { useSelector, shallowEqual } from 'react-redux'
+import { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+import { createSelector } from '@reduxjs/toolkit'
 import type { RootState } from '../../store/store'
 import { githubApi } from '../../api/githubApi'
-import type { GithubRepo } from '../../types/github'
+import type { GithubRepo, TrackedRepoRef } from '../../types/github'
+
+function makeSelectLiveRepos(trackedRefs: TrackedRepoRef[]) {
+  return createSelector(
+    (state: RootState) => state,
+    (state: RootState) =>
+      trackedRefs
+        .map((ref) => githubApi.endpoints.getRepoByFullName.select(ref.full_name)(state).data)
+        .filter((repo): repo is GithubRepo => repo !== undefined)
+  )
+}
 
 export function useTrackedRepoData(): GithubRepo[] {
-  return useSelector((state: RootState) => {
-    return state.tracked.repos
-      .map((ref) => githubApi.endpoints.getRepoByFullName.select(ref.full_name)(state).data)
-      .filter((repo): repo is GithubRepo => repo !== undefined)
-  }, shallowEqual)
+  const trackedRefs = useSelector((state: RootState) => state.tracked.repos)
+  const selectLiveRepos = useMemo(() => makeSelectLiveRepos(trackedRefs), [trackedRefs])
+  return useSelector(selectLiveRepos)
 }
